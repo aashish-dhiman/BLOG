2447b795a315d9f5f0dadd02e95838fb67eace8d
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-06-19 19:44:11 +0530

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
-    "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+    "Lorem ipsum dolor sit amet. Sed quia necessitatibus et velit dolorem est velit consectetur non laborum dolores et voluptas animi et molestias velit vel nihil dicta. Vel reprehenderit expedita et omnis sunt ea voluptatibus quas et deserunt pariatur et corrupti recusandae ut ducimus veritatis. 33 molestias similique sit voluptates voluptatem non illo doloremque est porro temporibus est explicabo voluptas qui sint similique qui porro ullam. Est aliquam omnis et magni beatae est voluptatum obcaecati sed corrupti ipsum et mollitia dolorum aut reprehenderit libero? Est nobis porro non mollitia minus qui facere aliquid. Ut optio porro est autem quas eum dicta inventore aut suscipit recusandae et commodi voluptatem ut dolor perferendis ut amet inventore.";
 const aboutContent =
     "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
@@ -18,9 +19,12 @@ app.use(express.static("public"));
 
 const PORT = 3000 || process.env.PORT;
 
+const posts = [];
+
 app.get("/", (req, res) => {
     res.render("home", {
         startingContent: homeStartingContent,
+        posts: posts,
     });
 });
 
@@ -38,6 +42,36 @@ app.get("/contact", (req, res) => {
 
 app.get("/compose", (req, res) => {
     res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+    // console.log(req.body.postTitle);
+    // console.log(req.body.postContent);
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postContent,
+    };
+
+    posts.push(post);
+    // console.log(posts);
+    res.redirect("/");
+});
+
+//express routing technique
+app.get("/posts/:postName", (req, res) => {
+    // console.log(req.params.postName);
+    const requestedTitle = _.lowerCase(req.params.postName);
+
+    posts.forEach((post) => {
+        let storedTitle = _.lowerCase(post.title);
+
+        if (requestedTitle === storedTitle) {
+            // console.log("Match found");
+            res.render("post", {
+                postData: post,
+            });
+        }
+    });
 });
 
 app.listen(PORT, function () {
