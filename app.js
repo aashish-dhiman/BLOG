f824f6615eb90178b6ba4ca5c8f92ffc60edd802
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-07-03 17:50:22 +0530

    added database integration and delete functionality

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
-
-const homeStartingContent =
-    "Lorem ipsum dolor sit amet. Sed quia necessitatibus et velit dolorem est velit consectetur non laborum dolores et voluptas animi et molestias velit vel nihil dicta. Vel reprehenderit expedita et omnis sunt ea voluptatibus quas et deserunt pariatur et corrupti recusandae ut ducimus veritatis. 33 molestias similique sit voluptates voluptatem non illo doloremque est porro temporibus est explicabo voluptas qui sint similique qui porro ullam. Est aliquam omnis et magni beatae est voluptatum obcaecati sed corrupti ipsum et mollitia dolorum aut reprehenderit libero? Est nobis porro non mollitia minus qui facere aliquid. Ut optio porro est autem quas eum dicta inventore aut suscipit recusandae et commodi voluptatem ut dolor perferendis ut amet inventore.";
-const aboutContent =
-    "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-    "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -17,27 +11,35 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
+const mongodb_URI =
+    "mongodb+srv://aashishdhiman88:Aashishd-mongodb@cluster0.9hytptp.mongodb.net/blogDB";
+
+mongoose.connect(mongodb_URI, {
+    useNewUrlParser: true,
+});
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+});
+
+const Post = mongoose.model("Post", blogSchema);
+
 const PORT = 3000 || process.env.PORT;
 
-const posts = [];
-
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    const posts = await Post.find({});
     res.render("home", {
-        startingContent: homeStartingContent,
         posts: posts,
     });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about", {
-        aboutContent: aboutContent,
-    });
+    res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact", {
-        contactContent: contactContent,
-    });
+    res.render("contact");
 });
 
 app.get("/compose", (req, res) => {
@@ -45,37 +47,33 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-    // console.log(req.body.postTitle);
-    // console.log(req.body.postContent);
-    const post = {
+    const post = new Post({
         title: req.body.postTitle,
         content: req.body.postContent,
-    };
+    });
+    post.save();
+    res.redirect("/");
+});
 
-    posts.push(post);
-    // console.log(posts);
+app.post("/delete", async (req, res) => {
+    const id = req.body.id;
+    
+    await Post.findByIdAndDelete(id);
     res.redirect("/");
 });
 
 //express routing technique
-app.get("/posts/:postName", (req, res) => {
-    // console.log(req.params.postName);
-    const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postID", async (req, res) => {
+    const postID = req.params.postID;
 
-    posts.forEach((post) => {
-        let storedTitle = _.lowerCase(post.title);
-
-        if (requestedTitle === storedTitle) {
-            // console.log("Match found");
-            res.render("post", {
-                postData: post,
-            });
-        }
+    const post = await Post.findById(postID).exec();
+    res.render("post", {
+        postData: post,
     });
 });
 
 app.listen(PORT, function () {
-    console.log("Server started on port 3000");
+    console.log("Server started on port " + PORT);
 });
 
 module.exports = app;
